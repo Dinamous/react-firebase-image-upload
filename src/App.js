3b933b2f8d5ee2eb3daec36f492cab4e94fe4cee
matheus simoes <matheusaugustosimoes@hotmail.com>
2020-08-10 01:40:53 -0300

    imagens aparecendo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
+import ImageGrid from './comps/ImageGrid';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Title/>
       <UploadForm/>
+      <ImageGrid/>
     </div>
   );
 }
