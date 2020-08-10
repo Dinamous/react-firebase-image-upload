4258845a80fe1498e4781694ba08bcaa04aa5d1f
matheus simoes <matheusaugustosimoes@hotmail.com>
2020-08-09 21:35:33 -0300

    inicio do projeto

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Suas Imagens</h2>
+      <p>Faça o upload das suas imagens aqui :=)</p>
+    </div>
+  )
+}
+
+export default Title;
